2b22afa5a7b5462d5ade8192fa2d330e9a04ee4a
dredegui <gui.pascoal@outlook.com>
2025-02-22 19:05:55 +0000

    feat: improve excel results formatting

--- human_study/ttest.py
+++ human_study/ttest.py
@@ -12,60 +12,52 @@ file_1_path = 'original_text_results.xlsx'
 data_0 = pd.read_excel(file_0_path)
 data_1 = pd.read_excel(file_1_path)
 
-# Function to extract numerical values from the "Total score" column
-def extract_score(score_str):
-    match = re.match(r"(\d+)", str(score_str))
-    return int(match.group(1)) if match else None
-
-# Apply extraction to the relevant columns in both datasets
-data_0['Total score'] = data_0['Total score'].apply(extract_score) / 15
-data_1['Total score'] = data_1['Total score'].apply(extract_score) / 15
-
 # Extract the scores for analysis
-enhanced_total_scores = data_0['Total score'].dropna()
-original_total_scores = data_1['Total score'].dropna()
+enhanced_total_scores = data_0['Total score %'].dropna()
+original_total_scores = data_1['Total score %'].dropna()
 
-enhanced_pre_text_scores = data_0['Pre-text Score %'].dropna()
-original_pre_text_scores = data_1['Pre-text Score %'].dropna()
+enhanced_pre_text_scores = data_0['Pre-text score %'].dropna()
+original_pre_text_scores = data_1['Pre-text score %'].dropna()
 
-enhanced_post_text_scores = data_0['Post-text Score %'].dropna()
-original_post_text_scores = data_1['Post-text Score %'].dropna()
+enhanced_post_text_scores = data_0['Post-text score %'].dropna()
+original_post_text_scores = data_1['Post-text score %'].dropna()
 
 # Calculate the mean and standard deviation for each set of scores
-mean_pre_text = enhanced_pre_text_scores.mean()
-mean_post_text = enhanced_post_text_scores.mean()
+enhanced_mean_stats = [enhanced_total_scores.mean(), enhanced_pre_text_scores.mean(), enhanced_post_text_scores.mean()]
+original_mean_stats = [original_total_scores.mean(), original_pre_text_scores.mean(), original_post_text_scores.mean()]
+enhanced_std_stats = [enhanced_total_scores.std(), enhanced_pre_text_scores.std(), enhanced_post_text_scores.std()]
+original_std_stats = [original_total_scores.std(), original_pre_text_scores.std(), original_post_text_scores.std()]
 
-std_pre_text = enhanced_pre_text_scores.std()
-std_post_text = enhanced_post_text_scores.std()
+
 
 print("Normality test for enhanced_pre_text_scores:", stats.shapiro(enhanced_pre_text_scores))
 print("Normality test for original_pre_text_scores:", stats.shapiro(original_pre_text_scores))
-
 print("Normality test for enhanced_post-text_scores:", stats.shapiro(enhanced_post_text_scores))
 print("Normality test for original_post-text_scores:", stats.shapiro(original_post_text_scores))
+print("Normality test for enhanced_total_scores:", stats.shapiro(enhanced_total_scores))
+print("Normality test for original_total_scores:", stats.shapiro(original_total_scores))
 
 # Perform independent t-tests
 t_total, p_total = ttest_ind(enhanced_total_scores, original_total_scores, equal_var=False)
 t_pre_text, p_pre_text = ttest_ind(enhanced_pre_text_scores, original_pre_text_scores, equal_var=False)
 t_post_text, p_post_text = ttest_ind(enhanced_post_text_scores, original_post_text_scores, equal_var=False)
 
+t_stats = [t_total, t_pre_text, t_post_text]
+p_stats = [p_total, p_pre_text, p_post_text]
+
 # Put results in a xlxs file
 results = {
-    'ttest total t-value': t_total,
-    'ttest total p-value': p_total,
-    'ttest pre-text t-value': t_pre_text,
-    'ttest pre-text p-value': p_pre_text,
-    'ttest post-text t-value': t_post_text,
-    'ttest post-text p-value': p_post_text,
-    'mean pre-text': mean_pre_text,
-    'mean post-text': mean_post_text,
-    'std pre-text': std_pre_text,
-    'std post-text': std_post_text
+    'Original Mean': original_mean_stats,
+    'Enhanced Mean': enhanced_mean_stats,
+    'Original Standard Deviation': original_std_stats,
+    'Enhanced Standard Deviation': enhanced_std_stats,
+    'T-Statistic': t_stats,
+    'P-Value': p_stats
 }
 
-df = pd.DataFrame(results, index=[0])
+df = pd.DataFrame(results, index=['Total', 'Pre-text', 'Post-text'])
 excel_path = 'ttest_results.xlsx'
-df.to_excel(excel_path, index=False)
+df.to_excel(excel_path, index=True)
 
 # Adjust column widths
 wb = load_workbook(excel_path)
